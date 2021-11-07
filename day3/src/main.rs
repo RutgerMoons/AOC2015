4b18d22166ece6c616c485622f54a0a933dbb4c0
= <=>
2021-11-07 17:49:11 +0100

    Day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,6 +4,7 @@ use std::collections::HashSet;
 
 #[derive(PartialEq, Eq, Hash)]
 struct Point(i32, i32);
+#[derive(Clone)]
 enum Instruction {
     Up, Down, Left, Right
 }
@@ -24,13 +25,9 @@ fn parse_instructions(input: &str) -> Vec<Instruction> {
     instr
 }
 
-fn solve(instr: &Vec<Instruction>) -> i64 {
-    let mut nb_houses: i64 = 1;
-    let mut visited: HashSet<Point> = HashSet::new();
-    visited.insert(Point(0, 0));
-
-    let mut x: i32 = 0;
-    let mut y: i32 = 0;
+fn visit(start: &Point, instr: &Vec<Instruction>, visited: &mut HashSet<Point>) {
+    let mut x: i32 = start.0;
+    let mut y: i32 = start.1;
     for ins in instr.iter() {
         match ins {
             Instruction::Up => y += 1,
@@ -40,16 +37,34 @@ fn solve(instr: &Vec<Instruction>) -> i64 {
         }
 
         if !visited.contains(&Point(x, y)) {
-            nb_houses += 1;
             visited.insert(Point(x, y));
         }
     }
-
-    nb_houses
 }
 
-fn solve_part_2(building: &str) -> Option<usize> {
-    unimplemented!()
+fn solve(instr: &Vec<Instruction>) -> usize {
+    let mut visited: HashSet<Point> = HashSet::new();
+    visited.insert(Point(0, 0));
+
+    visit(&Point(0, 0), instr, &mut visited);
+
+    visited.len()
+}
+
+fn solve_part_2(instr: &Vec<Instruction>) -> usize {
+    let mut visited: HashSet<Point> = HashSet::new();
+    visited.insert(Point(0, 0));
+
+    let santa_start = Point(0, 0);
+    let robot_start = Point(0, 0);
+
+    let santa_instr = instr.iter().enumerate().filter(|(idx, _)| idx % 2 == 0).map(|(_, ins)| ins.clone() ).collect();
+    let robot_instr = instr.iter().enumerate().filter(|(idx, _)| idx % 2 == 1).map(|(_, ins)| ins.clone() ).collect();
+
+    visit(&santa_start, &santa_instr, &mut visited);
+    visit(&robot_start, &robot_instr, &mut visited);
+
+    visited.len()
 }
 
 fn main() -> io::Result<()> {
@@ -64,9 +79,8 @@ fn main() -> io::Result<()> {
     let result = solve(&instr);
     println!("result part 1: {}", result);
 
-    if let Some(result) = solve_part_2(&line) {
-        println!("result part 2: {}", result);
-    }
+    let result = solve_part_2(&instr);
+    println!("result part 2: {}", result);
 
     Ok(())
 }
